50734130b572fbae9f58d79d50b83088c04b317f
devtofunmi <olayiwolajesutofunmi@gmail.com>
2025-10-15 16:12:18 +0100

    import navbar and chnage all imports to new folder name.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import SimulatedProfile from '../components/SimulatedProfile';
-import CallToAction from '../components/sections/CallToAction';
-import CrackedDevs from '../components/sections/CrackedDevs';
-import Features from '../components/sections/Features';
-import Footer from '../components/sections/Footer';
-import Hero from '../components/sections/Hero';
-import SocialProof from '../components/sections/SocialProof';
-import Testimonials from '../components/sections/Testimonials';
+import CallToAction from '../components/landingpage/CallToAction';
+import CrackedDevs from '../components/landingpage/CrackedDevs';
+import Features from '../components/landingpage/Features';
+import Footer from '../components/landingpage/Footer';
+import Hero from '../components/landingpage/Hero';
+import SocialProof from '../components/landingpage/SocialProof';
+import Testimonials from '../components/landingpage/Testimonials';
+import Navbar from '../components/landingpage/Navbar';
 
 const geistSans = { className: "font-sans" };
 const geistMono = { className: "font-mono" };
 
 const Home: React.FC = () => {
     return (
-        <div className={`${geistSans.className} ${geistMono.className} font-sans min-h-screen bg-[#f7f9fb] flex flex-col`}>
+        <div className={`${geistSans.className} ${geistMono.className} font-sans min-h-screen bg-white flex flex-col`}>
+            <Navbar />
             <Hero />
             <section className="bg-white px-6">
                 <SimulatedProfile />
