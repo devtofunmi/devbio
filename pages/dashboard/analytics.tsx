3252d2eefcb38232c5063d00560b45d17b9b618a
devtofunmi <olayiwolajesutofunmi@gmail.com>
2025-10-19 07:35:33 +0100

    analytics page

---
+++ pages/dashboard/analytics.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import DashboardLayout from '../../components/dashboard/DashboardLayout';
+
+const AnalyticsPage: React.FC = () => {
+  return (
+    <DashboardLayout>
+      <h1 className="text-2xl text-gray-700 font-bold">Analytics</h1>
+    </DashboardLayout>
+  );
+};
+
+export default AnalyticsPage;
