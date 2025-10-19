8ab47d0de258620169c50d048fc6cca56572fe98
devtofunmi <olayiwolajesutofunmi@gmail.com>
2025-10-19 07:34:41 +0100

    settings page

---
+++ pages/dashboard/settings.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import DashboardLayout from '../../components/dashboard/DashboardLayout';
+
+const SettingsPage: React.FC = () => {
+  return (
+    <DashboardLayout>
+      <h1 className="text-2xl text-gray-700 font-bold">Settings</h1>
+    </DashboardLayout>
+  );
+};
+
+export default SettingsPage;
