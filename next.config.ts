4ddf3f2bfd1dbe41d8a77a9bb6fc51a423af48c0
devtofunmi <olayiwolajesutofunmi@gmail.com>
2025-10-19 07:33:20 +0100

    added freepik

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'xsgames.co',
       },
+      {
+        protocol: 'https',
+        hostname: 'img.freepik.com',
+      },
     ],
   },
 };
