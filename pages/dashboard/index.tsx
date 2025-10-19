792d018d5d86cc00446fefb6aab1e186435b5024
devtofunmi <olayiwolajesutofunmi@gmail.com>
2025-10-19 07:36:13 +0100

    ..

---
+++ pages/dashboard/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DashboardLayout from '../../components/dashboard/DashboardLayout';
+
+const DashboardPage: React.FC = () => {
+  return (
+    <DashboardLayout>
+      <h1 className="text-2xl text-gray-700 font-bold">Your Page</h1>
+    </DashboardLayout>
+  );
+};
+
+export default DashboardPage;
