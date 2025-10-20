aadf9ea4ced8c97a163b080ea2eb1505adb5f935
devtofunmi <olayiwolajesutofunmi@gmail.com>
2025-10-20 15:31:05 +0100

    .

--- pages/dashboard/settings.tsx
+++ pages/dashboard/settings.tsx
@@ -1,11 +1,10 @@
-
 import React from 'react';
 import DashboardLayout from '../../components/dashboard/DashboardLayout';
 
 const SettingsPage: React.FC = () => {
   return (
     <DashboardLayout>
-      <h1 className="text-2xl text-gray-700 font-bold">Settings</h1>
+      <h1 className="text-2xl text-gray-700 font-bold mb-5">Settings</h1>
     </DashboardLayout>
   );
 };
