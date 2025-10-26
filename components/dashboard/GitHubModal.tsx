ed0522c8435e2538909034e3f6d008f674d2bb81
devtofunmi <olayiwolajesutofunmi@gmail.com>
2025-10-26 13:16:08 +0100

    changed modal styling

--- components/dashboard/GitHubModal.tsx
+++ components/dashboard/GitHubModal.tsx
@@ -1,5 +1,6 @@
 
 import React from "react";
+import { FaTimes } from "react-icons/fa";
 
 type GitHubModalProps = {
   githubUsername: string;
@@ -15,25 +16,24 @@ const GitHubModal: React.FC<GitHubModalProps> = ({
   handleSetGithubUsername,
 }) => {
   return (
-    <div className="fixed inset-0 bg-black/50 flex justify-center items-center p-4">
-      <div className="bg-white rounded-2xl shadow-2xl p-8 w-full max-w-md m-4">
-        <input
-          type="text"
-          value={githubUsername}
-          onChange={(e) => setGithubUsername(e.target.value)}
-          className="flex-1 p-3 border border-gray-200 rounded-lg bg-gray-50 focus:outline-none focus:ring-2 focus:ring-blue-400 text-black placeholder-gray-500 w-full"
-          placeholder="Enter your GitHub username"
-        />
-        <div className="flex justify-end space-x-4 mt-5">
-          <button
-            onClick={() => setGithubModalOpen(false)}
-            className="px-6 py-4 w-[200px] cursor-pointer bg-gray-100 text-center text-gray-800 rounded-full hover:bg-gray-200 transition-colors"
-          >
-            Cancel
-          </button>
+    <div className="fixed inset-0 bg-black/30 backdrop-blur-sm flex justify-center items-center p-4 z-50">
+      <div className="bg-white rounded-3xl shadow-2xl p-8 w-full max-w-lg m-4 relative transform transition-all duration-300 ease-in-out scale-100">
+        <button onClick={() => setGithubModalOpen(false)} className="absolute top-4 right-4 text-gray-400 hover:text-gray-600">
+          <FaTimes  />
+        </button>
+        <div className="flex items-center bg-gray-50 rounded-full p-1">
+          <input
+            type="text"
+            value={githubUsername}
+            onChange={(e) => setGithubUsername(e.target.value)}
+            className="flex-1 p-3 bg-transparent focus:outline-none text-black placeholder-gray-400 w-full"
+            placeholder="Enter your GitHub username"
+          />
+        </div>
+        <div className="flex justify-center space-x-4 mt-8">
           <button
             onClick={handleSetGithubUsername}
-            className="px-6 py-4 w-[200px] cursor-pointer bg-blue-400 text-white rounded-full hover:bg-blue-500 transition-colors shadow-md"
+            className="px-8 py-3 w-full cursor-pointer bg-blue-400 text-white text-center rounded-full hover:bg-blue-500 transition-colors duration-300 shadow-lg"
           >
             Set
           </button>
