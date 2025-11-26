96a2372de63f8790179ef8aab41f88db136da7b5
devtofunmi <olayiwolajesutofunmi@gmail.com>
2025-11-26 17:11:25 +0100

    fixed error

--- components/dashboard/DashboardLayout.tsx
+++ components/dashboard/DashboardLayout.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React  from "react";
 import Sidebar from "./Sidebar";
 
 const geistSans = { className: "font-sans" };
@@ -7,20 +7,15 @@ const geistMono = { className: "font-mono" };
 const DashboardLayout: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
-  const [isSidebarOpen, setSidebarOpen] = useState(true);
 
   return (
     <div
       className={`${geistSans.className} ${geistMono.className} font-sans min-h-screen bg-white flex`}
     >
       <Sidebar
-        isSidebarOpen={isSidebarOpen}
-        setSidebarOpen={setSidebarOpen}
       />
       <main
-        className={`flex-1 p-5 w-full md:pb-20 pb-16  ${
-          isSidebarOpen ? "lg:ml-80" : "lg:ml-0"
-        }`}
+        className="flex-1 p-5 w-full md:pb-20 pb-16"
       >
         
         {children}
