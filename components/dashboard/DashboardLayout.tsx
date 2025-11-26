7f649bc0710a3a2ed894ec9a16fd7850d44f7c00
devtofunmi <olayiwolajesutofunmi@gmail.com>
2025-11-26 16:02:05 +0100

    added sidebar

--- components/dashboard/DashboardLayout.tsx
+++ components/dashboard/DashboardLayout.tsx
@@ -1,14 +1,28 @@
-import React from 'react';
-import Sidebar from './Sidebar';
+import React, { useState } from "react";
+import Sidebar from "./Sidebar";
 
 const geistSans = { className: "font-sans" };
 const geistMono = { className: "font-mono" };
 
-const DashboardLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+const DashboardLayout: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [isSidebarOpen, setSidebarOpen] = useState(true);
+
   return (
-    <div className={`${geistSans.className} ${geistMono.className} font-sans min-h-screen bg-white flex`}>
-      <Sidebar />
-      <main className="flex-1 p-5 w-full md:pb-20 pb-16 lg:ml-80">
+    <div
+      className={`${geistSans.className} ${geistMono.className} font-sans min-h-screen bg-white flex`}
+    >
+      <Sidebar
+        isSidebarOpen={isSidebarOpen}
+        setSidebarOpen={setSidebarOpen}
+      />
+      <main
+        className={`flex-1 p-5 w-full md:pb-20 pb-16  ${
+          isSidebarOpen ? "lg:ml-80" : "lg:ml-0"
+        }`}
+      >
+        
         {children}
       </main>
     </div>
