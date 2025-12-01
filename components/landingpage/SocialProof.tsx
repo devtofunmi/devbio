aa822be07375eeedb082c22c2812411fc5df6ba1
devtofunmi <olayiwolajesutofunmi@gmail.com>
2025-12-01 11:01:45 +0100

    changed text and font size

--- components/landingpage/SocialProof.tsx
+++ components/landingpage/SocialProof.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 const SocialProof: React.FC = () => (
-    <section id="features" className="flex flex-col text-white items-center justify-center py-16 px-6 text-center">
-        <h3 className="text-4xl sm:text-5xl font-extrabold  mb-4">
+    <section id="social-proof" className="flex flex-col text-white items-center justify-center py-16 px-6 text-center">
+        <h3 className="text-4xl sm:text-5xl md:text-6xl font-extrabold mb-4">
             Your Code Repos. Analytics.
         </h3>
-        <h3 className="text-4xl sm:text-5xl font-extrabold  mb-8">
+        <h3 className="text-4xl sm:text-5xl md:text-6xl font-extrabold mb-8">
             Tech Stacks. Social Links.
         </h3>
-        <p className="text-2xl text-white max-w-2xl">
+        <p className="text-xl md:text-2xl text-white max-w-3xl">
             All your development content <span className="text-[#6f90fe]">integrated</span> into your personal page. No more hiding your work behind simple text links.
         </p>
     </section>
