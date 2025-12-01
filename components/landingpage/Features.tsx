f35eff9972134734b43d26857cdc13bf187bb5e1
devtofunmi <olayiwolajesutofunmi@gmail.com>
2025-12-01 11:01:24 +0100

    changed text and font size

--- components/landingpage/Features.tsx
+++ components/landingpage/Features.tsx
@@ -9,21 +9,21 @@ const Features: React.FC = () => (
         <div className="flex flex-col md:flex-row gap-8 lg:gap-12 items-center">
           <div className="md:w-5/12 w-full transform transition-transform duration-500 hover:scale-105">
             <div className="bg-white p-6 rounded-xl border border-gray-200 w-full">
-              <h3 className="font-bold text-2xl text-gray-800 mb-2">Project List</h3>
+              <h3 className="font-bold text-xl md:text-2xl text-gray-800 mb-2">Project List</h3>
               <div className="space-y-3 mt-4">
                 <div className="bg-gray-50 p-3 rounded-lg border border-gray-200">
-                  <p className="font-semibold text-xl text-gray-700">Project Alpha</p>
+                  <p className="font-semibold text-lg md:text-xl text-gray-700">Project Alpha</p>
                 </div>
                 <div className="bg-gray-50 p-3 rounded-lg border border-gray-200">
-                  <p className="font-semibold text-xl text-gray-700">Project Beta</p>
+                  <p className="font-semibold text-lg md:text-xl text-gray-700">Project Beta</p>
                 </div>
               </div>
             </div>
           </div>
           <div className="md:w-7/12 w-full">
-            <h3 className="font-bold text-5xl text-[#cee933] mb-3">Showcase Your Best Work</h3>
-            <p className="text-white leading-relaxed text-2xl">
-              Go beyond a simple list of projects. With DevBio, you can create a rich, detailed portfolio. Add descriptions, tech stacks, and live links. Embed images and videos to give recruiters and collaborators a comprehensive look at what you&apos;ve built.
+            <h3 className="font-bold text-4xl md:text-5xl text-[#cee933] mb-3">Showcase Your Best Work</h3>
+            <p className="text-white leading-relaxed text-xl md:text-2xl">
+              Go beyond a simple list of projects. With DevBio, you can create a rich, detailed portfolio. Add descriptions, tech stacks, and live links.
             </p>
           </div>
         </div>
@@ -36,7 +36,7 @@ const Features: React.FC = () => (
         <div className="flex flex-col md:flex-row-reverse gap-8 lg:gap-12 items-center">
           <div className="md:w-5/12 w-full transform transition-transform duration-500 hover:scale-105">
             <div className="bg-slate-50 p-6 rounded-xl border border-gray-200 w-full">
-              <h3 className="font-bold text-2xl text-gray-800 mb-2">Built-in Analytics</h3>
+              <h3 className="font-bold text-xl md:text-2xl text-gray-800 mb-2">Built-in Analytics</h3>
               <div className="bg-white p-3 rounded-lg border border-gray-200 h-32 flex items-end space-x-2 mt-4">
                 <div className="w-1/4 bg-blue-400 h-1/3 rounded-t-md"></div>
                 <div className="w-1/4 bg-blue-400 h-2/3 rounded-t-md"></div>
@@ -46,8 +46,8 @@ const Features: React.FC = () => (
             </div>
           </div>
           <div className="md:w-7/12 w-full">
-            <h3 className="font-bold text-5xl text-blue-400 mb-3">Understand Your Audience</h3>
-            <p className="text-gray-800 leading-relaxed text-2xl">
+            <h3 className="font-bold text-4xl md:text-5xl text-blue-400 mb-3">Understand Your Audience</h3>
+            <p className="text-black leading-relaxed text-xl md:text-2xl">
               Knowledge is power. Our built-in analytics give you valuable insights into your profile&apos;s performance. See how many people are viewing your page, where they&apos;re coming from, and what content is most popular. Use this data to tailor your profile and grow your online presence.
             </p>
           </div>
@@ -61,7 +61,7 @@ const Features: React.FC = () => (
         <div className="flex flex-col md:flex-row gap-8 lg:gap-12 items-center">
           <div className="md:w-5/12 w-full transform transition-transform duration-500 hover:scale-105">
             <div className="bg-white p-6 rounded-xl border border-gray-200 w-full">
-              <h3 className="font-bold text-2xl text-gray-800 mb-2">GitHub Contributions</h3>
+              <h3 className="font-bold text-xl md:text-2xl text-gray-800 mb-2">GitHub Contributions</h3>
               <div className="mt-4 grid grid-flow-col grid-rows-7 gap-1">
                 {Array.from({ length: 182 }).map((_, i) => (
                   <div
@@ -75,9 +75,9 @@ const Features: React.FC = () => (
             </div>
           </div>
           <div className="md:w-7/12 w-full">
-            <h3 className="font-bold text-5xl text-[#cee933] mb-3">Sync Your GitHub Activity</h3>
-            <p className="text-white leading-relaxed text-2xl">
-              Keep your profile dynamic and up-to-date automatically. DevBio seamlessly integrates with your GitHub account to display your contribution graph and recent activity. It&rsquo;s the easiest way to show you&apos;re an active and engaged developer.
+            <h3 className="font-bold text-4xl md:text-5xl text-[#cee933] mb-3">Sync Your GitHub Activity</h3>
+            <p className="text-white leading-relaxed text-xl md:text-2xl">
+              Keep your profile dynamic and up-to-date automatically. DevBio seamlessly integrates with your GitHub account to display your contribution graph. It&rsquo;s the easiest way to show you&apos;re an active and engaged developer.
             </p>
           </div>
         </div>
@@ -90,8 +90,8 @@ const Features: React.FC = () => (
         <div className="flex flex-col md:flex-row-reverse gap-8 lg:gap-12 items-center">
           <div className="md:w-5/12 w-full transform transition-transform duration-500 hover:scale-105">
             <div className="bg-slate-50 p-6 rounded-xl border border-gray-200 w-full">
-              <h3 className="font-bold text-2xl text-gray-800 mb-2">Tech Stack Visuals</h3>
-              <div className="flex justify-around items-center h-24 text-5xl text-gray-500 mt-4">
+              <h3 className="font-bold text-xl md:text-2xl text-gray-800 mb-2">Tech Stack Visuals</h3>
+              <div className="flex justify-around items-center h-24 text-4xl md:text-5xl text-gray-500 mt-4">
                 <FaReact className="hover:text-blue-500" />
                 <FaNodeJs className="hover:text-green-500" />
                 <FaDatabase className="hover:text-purple-500" />
@@ -101,9 +101,9 @@ const Features: React.FC = () => (
             </div>
           </div>
           <div className="md:w-7/12 w-full">
-            <h3 className="font-bold text-5xl text-white mb-3">Display Your Expertise</h3>
-            <p className="text-gray-300 leading-relaxed text-2xl">
-              Don&apos;t just list your skills—show them. Our visual tech stack allows you to display the languages, frameworks, and tools you&apos;re proficient in. It&rsquo;s a quick and effective way for visitors to gauge your technical abilities.
+            <h3 className="font-bold text-4xl md:text-5xl text-white mb-3">Display Your Expertise</h3>
+            <p className="text-gray-300 leading-relaxed text-xl md:text-2xl">
+              Don&apos;t just list your skills show them. Our visual tech stack allows you to display the languages, frameworks, and tools you&apos;re proficient in. It&rsquo;s a quick and effective way for visitors to gauge your technical abilities.
             </p>
           </div>
         </div>
@@ -116,7 +116,7 @@ const Features: React.FC = () => (
         <div className="flex flex-col md:flex-row gap-8 lg:gap-12 items-center">
           <div className="md:w-5/12 w-full transform transition-transform duration-500 hover:scale-105">
             <div className="bg-white p-6 rounded-xl border border-gray-200 w-full">
-              <h3 className="font-bold text-2xl text-gray-800 mb-2">Customizable Profile</h3>
+              <h3 className="font-bold text-xl md:text-2xl text-gray-800 mb-2">Customizable Profile</h3>
               <div className="bg-gray-50 p-4 mt-4 rounded-lg border border-gray-200 flex items-center space-x-3">
                 <div className="w-12 h-12 rounded-full bg-gray-300"></div>
                 <div>
@@ -127,8 +127,8 @@ const Features: React.FC = () => (
             </div>
           </div>
           <div className="md:w-7/12 w-full">
-            <h3 className="font-bold text-5xl text-white mb-3">Make It Uniquely Yours</h3>
-            <p className="text-gray-200 leading-relaxed text-2xl">
+            <h3 className="font-bold text-4xl md:text-5xl text-white mb-3">Make It Uniquely Yours</h3>
+            <p className="text-gray-200 leading-relaxed text-xl md:text-2xl">
               Your personal brand matters. Choose from multiple themes and layouts to create a profile that reflects your style. Make a memorable first impression that sets you apart from the crowd.
             </p>
           </div>
@@ -142,15 +142,15 @@ const Features: React.FC = () => (
         <div className="flex flex-col md:flex-row-reverse gap-8 lg:gap-12 items-center">
           <div className="md:w-5/12 w-full transform transition-transform duration-500 hover:scale-105">
             <div className="bg-slate-50 p-6 rounded-xl border border-gray-200 w-full">
-              <h3 className="font-bold text-2xl text-gray-800 mb-2">Custom Domains</h3>
+              <h3 className="font-bold text-xl md:text-2xl text-gray-800 mb-2">Custom Domains</h3>
               <div className="bg-white p-3 mt-4 rounded-lg border border-gray-200">
-                <p className="text-center font-mono text-blue-400 text-3xl">yourname.dev</p>
+                <p className="text-center font-mono text-blue-400 text-2xl md:text-3xl">yourname.dev</p>
               </div>
             </div>
           </div>
           <div className="md:w-7/12 w-full">
-            <h3 className="font-bold text-5xl text-white mb-3">Own Your Online Identity</h3>
-            <p className="text-gray-100 leading-relaxed text-2xl">
+            <h3 className="font-bold text-4xl md:text-5xl text-white mb-3">Own Your Online Identity</h3>
+            <p className="text-gray-100 leading-relaxed text-xl md:text-2xl">
               Strengthen your professional brand by linking your profile to your own custom domain. A personal domain like `yourname.dev` makes your portfolio more professional, easier to share, and more likely to be remembered.
             </p>
           </div>
