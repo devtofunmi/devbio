108dfe3140c823e34c3f6591f3f57603b293431c
devtofunmi <olayiwolajesutofunmi@gmail.com>
2026-01-09 11:20:29 +0100

    wrap app with authcontext

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
 import "@/styles/globals.css";
 import "keen-slider/keen-slider.min.css";
 import type { AppProps } from "next/app";
+import { AuthProvider } from "@/lib/AuthContext";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
 }
