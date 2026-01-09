0633c6c9c56bd00599d518df64afdafd6cad0533
devtofunmi <olayiwolajesutofunmi@gmail.com>
2026-01-09 12:06:37 +0100

    import and added toastify container

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import "@/styles/globals.css";
 import "keen-slider/keen-slider.min.css";
+import 'react-toastify/dist/ReactToastify.css';
 import type { AppProps } from "next/app";
 import { AuthProvider } from "@/lib/AuthContext";
+import { ToastContainer } from 'react-toastify';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
       <Component {...pageProps} />
+      <ToastContainer position="top-right" autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover theme="dark" />
     </AuthProvider>
   );
 }
