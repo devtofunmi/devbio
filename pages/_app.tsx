1967893b57baa3cac1cb05e9aa8ae88da02b01d2
devtofunmi <olayiwolajesutofunmi@gmail.com>
2026-01-10 18:50:51 +0100

    Wrap application with `SessionContextProvider` and `AuthProvider` in `_app.tsx` to persist auth state.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,25 @@
 import "@/styles/globals.css";
 import "keen-slider/keen-slider.min.css";
 import 'react-toastify/dist/ReactToastify.css';
+import React, { useState } from 'react';
 import type { AppProps } from "next/app";
 import { AuthProvider } from "@/lib/AuthContext";
 import { ToastContainer } from 'react-toastify';
+import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs';
+import { SessionContextProvider } from '@supabase/auth-helpers-react';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [supabaseClient] = useState(() => createPagesBrowserClient());
+
   return (
-    <AuthProvider>
-      <Component {...pageProps} />
-      <ToastContainer position="top-right" autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover theme="dark" />
-    </AuthProvider>
+    <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}
+    >
+      <AuthProvider>
+        <Component {...pageProps} />
+        <ToastContainer position="top-right" autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover theme="dark" />
+      </AuthProvider>
+    </SessionContextProvider>
   );
 }
