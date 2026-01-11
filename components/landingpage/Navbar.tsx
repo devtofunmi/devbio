7951589854bb68694d97b73db53177fc443322cf
devtofunmi <olayiwolajesutofunmi@gmail.com>
2026-01-11 02:02:23 +0100

    Improved 'Navbar' behavior by disabling background scrolling while the mobile menu is open.

--- components/landingpage/Navbar.tsx
+++ components/landingpage/Navbar.tsx
@@ -17,6 +17,17 @@ const Navbar: React.FC = () => {
         return () => window.removeEventListener('scroll', handleScroll);
     }, []);
 
+    useEffect(() => {
+        if (isMenuOpen) {
+            document.body.style.overflow = 'hidden';
+        } else {
+            document.body.style.overflow = 'unset';
+        }
+        return () => {
+            document.body.style.overflow = 'unset';
+        };
+    }, [isMenuOpen]);
+
     return (
         <>
             <motion.header
