e867d899feb81e99f1ab4de5fef566da8d838968
devtofunmi <olayiwolajesutofunmi@gmail.com>
2026-01-12 13:49:42 +0100

    added a floating animation to the share button

--- pages/[profile].tsx
+++ pages/[profile].tsx
@@ -194,10 +194,17 @@ const ProfilePage: React.FC<Props> = ({ user, projects }) => {
 
       {/* Floating Share Button - Premium Glassmorphism Neon Version */}
       <motion.div
-        initial={{ opacity: 0, scale: 0.5, y: 50 }}
-        animate={{ opacity: 1, scale: 1, y: 0 }}
-        whileHover={{ scale: 1.05 }}
-        whileTap={{ scale: 0.95 }}
+        animate={{
+          y: [0, -12, 0],
+        }}
+        transition={{
+          duration: 4,
+          repeat: Infinity,
+          ease: "easeInOut"
+        }}
+        whileHover={{ scale: 1.1, y: 0 }}
+        whileTap={{ scale: 0.9 }}
+
         className="fixed bottom-8 right-8 md:bottom-12 md:right-12 z-[100]"
       >
         <button
